745e71e00f9689241b99ac59677b35e6b9b16e4f
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2025-07-09 13:41:23 +0530

    Create subnet.tf

---
+++ terraform/modules/azure/subnet.tf
@@ -0,0 +1,6 @@
+resource "azurerm_subnet" "main" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.subnet_address_prefixes
+}
