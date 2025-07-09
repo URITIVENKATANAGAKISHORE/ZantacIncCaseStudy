e1b26da0e7a16f067accffe122c96a9ae8906f62
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2025-07-09 13:35:08 +0530

    Create subnet.tf

---
+++ terraform/modules/aws/subnet.tf
@@ -0,0 +1,25 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone
+  tags = {
+    Name = "${var.environment}-public-subnet"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
