d5ddd669450d7935effb68bedb8ebcf265a19799
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2025-07-11 17:51:15 +0530

    Update data.tf

--- terraform/modules/aws/data.tf
+++ terraform/modules/aws/data.tf
@@ -13,3 +13,17 @@ data "aws_ami" "amazon_linux" {
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+// Data sources
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
