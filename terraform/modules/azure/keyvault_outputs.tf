e59821b83eba01fca2662cb259a1ce0118722f8a
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2025-07-11 17:53:27 +0530

    Delete terraform/modules/azure/keyvault_outputs.tf

--- terraform/modules/azure/keyvault_outputs.tf
+++
@@ -1,9 +0,0 @@
-output "key_vault_uri" {
-  value = azurerm_key_vault.main.vault_uri
-}
-output "admin_username_secret_id" {
-  value = azurerm_key_vault_secret.admin_username.id
-}
-output "admin_password_secret_id" {
-  value = azurerm_key_vault_secret.admin_password.id
-}
