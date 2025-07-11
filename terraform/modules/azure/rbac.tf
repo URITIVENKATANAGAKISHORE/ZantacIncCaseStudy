6b1217e91d838130f9080b154cafec0affe9d325
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2025-07-11 17:54:03 +0530

    Delete terraform/modules/azure/rbac.tf

--- terraform/modules/azure/rbac.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "vmss_contributor" {
-  scope                = azurerm_resource_group.main.id
-  role_definition_name = "Contributor"
-  principal_id         = azurerm_linux_virtual_machine_scale_set.main.identity[0].principal_id
-}
